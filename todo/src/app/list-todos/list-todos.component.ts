1540581a6e9c51a04a77517884e15fe4e33a8ee4
Abhishek <abhishek200men@gmail.com>
2024-04-15 23:40:41 +0530

    Added todo class , and added Pipe

--- todo/src/app/list-todos/list-todos.component.ts
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -1,23 +1,35 @@
-import { NgFor } from '@angular/common';
+import { NgIf, NgFor, UpperCasePipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgIf, NgFor, UpperCasePipe, DatePipe],
   templateUrl: './list-todos.component.html',
   styleUrl: './list-todos.component.css'
 })
 export class ListTodosComponent {
 
   todos = [
-    {id:1,desc:'Learn to  move'},
-    {id:2,desc:'angular expert'},
-    {id:3,desc:'visit world'}
+    new Todo(1,'Learn to  move',false,new Date()),
+    new Todo(2,'angular expert',false,new Date()),
+    new Todo(3,'visit world',false,new Date())
   ];
   // todo = {
   //   id : 1,
   //   description : 'learn to move'
   // };
 
+}
+
+export class Todo {
+  
+  constructor(
+    public id: number,
+    public desc: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
 }
