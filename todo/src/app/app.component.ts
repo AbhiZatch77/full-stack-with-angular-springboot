f21b06ab748253577af856e852205ce4f6abc4ec
Abhishek <abhishek200men@gmail.com>
2024-04-16 19:36:30 +0530

    Added Footer and menu component

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent,RouterOutlet],
+  imports: [LoginComponent,MenuComponent,FooterComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
