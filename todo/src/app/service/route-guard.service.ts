c2da5ae2f4b4726bd34ca8f2db318c022434f377
Abhishek <abhishek200men@gmail.com>
2024-04-16 23:53:39 +0530

    Added routeGuard service so that directly typing url can't take to a specifc page wothout login.

---
+++ todo/src/app/service/route-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  GuardResult,
+  MaybeAsync,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RouteGuardService implements CanActivate {
+  constructor(
+    private hardcodedAuthenticationService: HardcodedAuthenticationService
+  ) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.hardcodedAuthenticationService.isUserLoggedIn();
+  }
+}
